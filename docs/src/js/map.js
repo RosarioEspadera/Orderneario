12bb7cd9f3172df6d1f99c0ba3538ebaafd3ebe5
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-19 02:45:08 +0800

    Update map.js

--- docs/src/js/map.js
+++ docs/src/js/map.js
@@ -1,7 +1,7 @@
 // src/js/map.js
 import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://roqikwfaenwqipdydhwv.supabase.co';
+const SUPABASE_URL = 'https://neigxicrhalonnsaqkud.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvcWlrd2ZhZW53cWlwZHlkaHd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MTYxMzksImV4cCI6MjA2ODE5MjEzOX0.CpUCA3X4bNIjOCtxrdOZ2kciXEHEogukBie9IOlHpno'; // truncated
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
