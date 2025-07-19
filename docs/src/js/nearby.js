8b849a8e67c0cc6258f92c1bc2f57f8002b00fb2
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-19 16:43:38 +0800

    Update nearby.js

--- docs/src/js/nearby.js
+++ docs/src/js/nearby.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 
-const supabase = createClient('https://neigxicrhalonnsaqkud.supabase.co', 'your-anon-key');
+const supabase = createClient('https://neigxicrhalonnsaqkud.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5laWd4aWNyaGFsb25uc2Fxa3VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4NDQ3NjcsImV4cCI6MjA2ODQyMDc2N30.43DDOz-38NSc0nUejfTGOMD4xYBfzNvy4n0NFZWEfeo');
 
 (async () => {
   // Get user's geolocation
