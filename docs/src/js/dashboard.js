308a04d863bd7c383cc57cea7e03db7ef21348ae
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-20 00:54:18 +0800

    Update dashboard.js

--- docs/src/js/dashboard.js
+++ docs/src/js/dashboard.js
@@ -1,8 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://neigxicrhalonnsaqkud.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5laWd4aWNyaGFsb25uc2Fxa3VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4NDQ3NjcsImV4cCI6MjA2ODQyMDc2N30.43DDOz-38NSc0nUejfTGOMD4xYBfzNvy4n0NFZWEfeo'; // Replace with secure env later
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+import { supabase } from './supabaseClient.js';
 
 let userId, userRole, map;
 let storeMarkers = {};
