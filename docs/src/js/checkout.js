a5371bf920a350b30b6e1634f78c07675a0fc38d
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-20 21:44:15 +0800

    Update checkout.js

--- docs/src/js/checkout.js
+++ docs/src/js/checkout.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 const supabase = createClient('https://neigxicrhalonnsaqkud.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5laWd4aWNyaGFsb25uc2Fxa3VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4NDQ3NjcsImV4cCI6MjA2ODQyMDc2N30.43DDOz-38NSc0nUejfTGOMD4xYBfzNvy4n0NFZWEfeo');
 
-emailjs.init('espaderarosario07@gmail.com'); // Replace with your EmailJS user ID
+emailjs.init('AqvkFhQnxowOJda9J'); // Replace with your EmailJS user ID
 
 const orderList = document.getElementById('orderList');
 const totalEl = document.getElementById('totalAmount');
